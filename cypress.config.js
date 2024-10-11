ae391523770172ff8eea527cefdd98f1ca504e65
Sunny BIARD <sunny.biard@gmail.com>
2024-10-11 13:04:17 +0200

    soutenance

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+
+  reporterOptions: {
+    charts: true,
+    json: true,
+    reportFilename: "cypress-report",
+    overwrite: false,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
